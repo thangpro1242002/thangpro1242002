339f32f816d3dd5da3fc45e984f795bf27c918b0
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2020-11-23 18:20:37 +0700

    Add files via upload

---
+++ bai8.py
@@ -0,0 +1,10 @@
+x = input("địa chỉ email muốn gửi thư: ")
+y = input("thông điệp gửi thư: ")
+z = input("số lần gửi thư: ")
+answer=1
+n=int(input("nhập vào số lần lặp: "))
+while answer <= n:
+       answer=answer+1
+       print("địa chỉ email muốn gửi thư: ",x)
+       print("thông điệp muốn gửi thư:     ",y)
+       print("số lần gửi thư:              ",z)
