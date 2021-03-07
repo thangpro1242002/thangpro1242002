5215e18d322b9bbf37089a8a00caf710fc36e282
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-03-07 15:26:25 +0700

    Update random.py

--- pythonProject/random.py
+++ pythonProject/random.py
@@ -1,6 +1,6 @@
 import pandas as pd
-data = pd.read_csv("danh-sach-sinh-vien - Trang tính1.csv")
-sinhvien = data.sample(7)
+file = pd.read_csv("danh-sach-sinh-vien - Trang tính1.csv")
+sinhvien = file.sample(7)
 sinhvien.to_csv('7sinhvienngaunhien.csv',index=False)
 
 
