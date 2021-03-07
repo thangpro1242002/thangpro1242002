cc17642c366b2fc098e2b8798ff06338fbddf411
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-03-07 07:24:39 +0700

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data = pd.read_csv("danh-sach-sinh-vien - Trang tính1.csv")
 sinhvien = data.sample(7)
-sinhvien.to_csv('7sinhvien.csv',index=False)
+sinhvien.to_csv('7sinhvienngaunhien.csv',index=False)
 
 
 
